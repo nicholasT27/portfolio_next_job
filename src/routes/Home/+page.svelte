86c8a8ce0969425d0b02a47f91b2ae4f4b89edf2
kee-siang <tksiang.ss@example.com>
2023-08-09 23:43:52 +0800

    modify login route

--- src/routes/Home/+page.svelte
+++ src/routes/Home/+page.svelte
@@ -87,7 +87,7 @@
   <!-- Right-hand side content -->
   <div class="p-4 flex items-center h-24">
     <a href="/post/new" class="block rounded-full px-4 py-2 text-base text-gray-200 hover:text-black hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Post Job</a>
-    <a href="/log-in" class="block rounded-full px-4 py-2 text-base text-gray-200 hover:text-black hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Log In</a>
+    <a href="/login" class="block rounded-full px-4 py-2 text-base text-gray-200 hover:text-black hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Log In</a>
     <a href="/sign-up" class="block rounded-full px-4 py-2 text-base text-gray-200 hover:text-black hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign Up</a>
   </div>
 
