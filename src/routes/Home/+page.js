2615cb1c4eae7b263ae917269fa8c4425894bdbc
kee-siang <tksiang.ss@example.com>
2023-09-04 23:37:10 +0800

    pagination configuration

--- src/routes/Home/+page.js
+++ src/routes/Home/+page.js
@@ -2,7 +2,7 @@ import { PUBLIC_BACKEND_BASE_URL } from '$env/static/public';
 
 //use to fetch data from the database through 'api/collections/jobs/records'//
 export async function load({ fetch }) {
-    const resp = await fetch(PUBLIC_BACKEND_BASE_URL + 'api/collections/jobs/records');
+    const resp = await fetch(PUBLIC_BACKEND_BASE_URL + 'api/collections/jobs/records?page=1&perPage=10');
     
     const res = await resp.json();
     if (resp.status == 200) {
