d752b7e79694f7d6877a61eea508d188cb933f20
Nicholas Teoh <nicholasteoh@Nicholass-MacBook-Air.local>
2026-09-21 16:13:11 +0800

    Refine home responsiveness and remove loader

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,1 +1,6 @@
-export const prerender = true;
+import { redirect } from '@sveltejs/kit';
+
+// The root route goes straight to the working Home experience. No loading screen is shown.
+export function load() {
+	throw redirect(307, '/Home');
+}
